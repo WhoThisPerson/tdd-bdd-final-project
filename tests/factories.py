c1648d354a6285bf6df08d088d8a39c72ffdb43b
Qirong <qirongwu18@gmail.com>
2025-11-09 17:12:50 -0500

    Fixing spelling error

--- tests/factories.py
+++ tests/factories.py
@@ -38,7 +38,7 @@ class ProductFactory(factory.Factory):
 
     description = factory.Faker("text")
     price = FuzzyDecimal(0.5, 2000.0, 2)
-    avaialble = FuzzyChoice(choices=[True, False])
+    available = FuzzyChoice(choices=[True, False])
     category = FuzzyChoice(
         choices = [Category.UNKNOWN, Category.CLOTHS, Category.FOOD, Category.HOUSEWARES, Category.AUTOMOTIVE, Category.TOOLS]
     )
